e42fd10c8d76f988cf17b3014b5321ffdc4e7148
Komal Rathi <komalsrathi@users.noreply.github.com>
2015-09-01 15:51:52 -0400

    prepare GTEx data for downstream analysis

--- prepare_GTEx.R
+++ prepare_GTEx.R
@@ -2,8 +2,19 @@ library(reshape2)
 library(data.table)
 
 # download the raw files from GTEx
-gtex <- fread('GTEx_Analysis_2014-01-17_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct')
+gtex <- fread('GTEx_Analysis_2014-01-17_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct') # rpkm values or
+gtex <- fread('GTEx_Analysis_2014-01-17_RNA-seq_RNA-SeQCv1.1.8_gene_reads.gct') # raw counts
+
+# melt counts/expression values
 gtex.melt <- melt(gtex,id.vars=c("Name","Description"),variable.name="SAMPID")
+
+# get annotation
 gtex.ann <- fread('GTEx_Data_2014-01-17_Annotations_SampleAttributesDS.txt')
+gtex.ann <- gtex.ann[, .(SAMPID,SMTS,SMTSD)]
+
+# merge counts/rpkm with annotation
 dat <- merge(gtex.ann,gtex.melt,by="SAMPID")
-dat <- dcast(dat, SAMPID + SMTS + SMTSD ~ Description, value.var="value")
+
+# after this, you can extract any tissue type and do downstream analysis
+dt <-  subset(dat, SMTSD == "Whole Blood")
+# dt <- dcast(dt, SAMPID + SMTS + SMTSD ~ Description, value.var="value")
