1a205892cf8873ccbb2329f42343003744c501ad
Komal Rathi <komalsrathi@users.noreply.github.com>
2015-09-01 15:19:35 -0400

    prepare GTEx data for downstream analysis

--- prepare_GTEx.R
+++ prepare_GTEx.R
@@ -4,6 +4,6 @@ library(data.table)
 # download the raw files from GTEx
 gtex <- fread('GTEx_Analysis_2014-01-17_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct')
 gtex.melt <- melt(gtex,id.vars=c("Name","Description"),variable.name="SAMPID")
-gtex.ann <- read.delim('GTEx_Data_2014-01-17_Annotations_SampleAttributesDS.txt')
+gtex.ann <- fread('GTEx_Data_2014-01-17_Annotations_SampleAttributesDS.txt')
 dat <- merge(gtex.ann,gtex.melt,by="SAMPID")
 dat <- dcast(dat, SAMPID + SMTS + SMTSD ~ Description, value.var="value")
